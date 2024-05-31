870d0af0e75d6e1f0305ebda88a2b6d4de5a2c79
Felipealvesn <felposoalves@gmail.com>
2024-05-30 22:20:05 -0300

    site atualizado

---
+++ script.js
@@ -0,0 +1,10 @@
+function calcularDolar(){
+
+let real = document.getElementById('reais').value ;
+let dolar = document.getElementById('dolares').value ;
+
+let resultado = ("o resultado da conversão do real para o dolar é de: " + real / dolar + " dolares")
+
+alert(resultado)
+
+}
